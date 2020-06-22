c32dcfa45314b447503f53d1044168991862935e
StefanYohansson <sy.fen0@gmail.com>
2020-06-22 03:14:39 +0100

    Filter meh

--- src/components/Management.js
+++ src/components/Management.js
@@ -14,6 +14,7 @@ const Management = props => {
     const [ isOpen, setOpen ] = React.useState(false);
     const [ data, setData ] = React.useState(null);
     const [ groups, setGroups ] = React.useState(null);
+    const [ filter, setFilter ] = React.useState({});
     
     const closeModal = () => {
         setOpen(false);
@@ -28,13 +29,15 @@ const Management = props => {
     React.useEffect(() => {
         async function fetchData() {
             const res = await fetch(api.getAllUsers).then(res => res.json());
-            setGroups(_.groupBy(res.users, 'group_id'));
+            setGroups(_.groupBy(_.orderBy(res.users, ['active'], ['desc']), 'group_id'));
         }
         
         fetchData();
-    }, [setGroups]);
+    }, []);
 
     if (!groups) return <Loader />;
+
+    const debouncedSetFilter = _.debounce((input, key) => setFilter({ ...filter, [key]: input.target.value || undefined }), 500);
 
     return (
         <React.Fragment>
@@ -43,6 +46,20 @@ const Management = props => {
                 data={data}
                 closeModal={closeModal}
             />
+            <label htmlFor="filter_name">
+              Filtro Nome
+            </label><input type="text" onChange={event => {
+              event.persist();
+              return debouncedSetFilter(event, "user.name"); }} name="filter_name"/>
+            <label htmlFor="filter_number">
+              Filtro Número 
+            </label><input type="text" onChange={event => {
+              event.persist();
+              debouncedSetFilter(event, "phone_number"); }} name="filter_number"/>
+            <button className="button is-primary" onClick={() => setFilter({ ...filter, active: true })}>Filtrar ativos</button>
+            <button className="button is-danger" onClick={() => setFilter({ ...filter, active: false })}>Filtrar inativos</button>
+            <button className="button" onClick={() => setFilter({ ...filter, active: undefined })}>Mostrar ativos e inativos</button>
+            <button className="button" onClick={() => setFilter({})}>Limpar</button>
             {_.map(groups, (users, key) => {
                 return (
                     <React.Fragment key={key}>
@@ -50,6 +67,7 @@ const Management = props => {
                             users={users}
                             groupId={key}
                             onDelete={onDelete}
+                            filter={filter}
                         />
                     </React.Fragment>
                 );
@@ -68,6 +86,7 @@ const Trigger = (productName) => (opened) => (
 const CollapsibleTable = ({
     groupId,
     users,
+    filter,
     onDelete
 }) => {
     const productName = groupId === '-1001241265705' ? 'COMUNIDADE TOPMAC DE SINAIS E ACOMPANHAMENTOS' : 'Grupo de teste';
@@ -80,13 +99,23 @@ const CollapsibleTable = ({
             trigger={Trigger(productName)(false)}
             triggerWhenOpen={Trigger(productName)(true)}
             key={groupId}>
-            <GroupTable users={users} id={groupId} onDelete={onDelete} />
+            <GroupTable users={users} id={groupId} onDelete={onDelete} filter={filter} />
         </Collapsible>
     )
 };
 
-const GroupTable = ({ users, id, onDelete }) => {
-    return (
+const applyFilter = (user, _filters) => {
+  const filters = _.reduce(_filters, (acc, value, index) => {
+    if (value === undefined) {
+      delete acc[index];
+    }
+    return acc;
+  }, _filters);
+  return Object.keys(filters).length === 0 ? true : Object.keys(filters).some(filterKey => _.isBoolean(_.get(user, filterKey)) ? filters[filterKey] === _.get(user, filterKey) : _.get(user, filterKey) && _.get(user, filterKey).toLowerCase().includes((filters[filterKey] || "").toLowerCase()));
+};
+
+const GroupTable = ({ users, id, filter, onDelete }) => {
+  return (
         <div className="container">
             <table className="table group-table">
                 <thead>
@@ -98,7 +127,7 @@ const GroupTable = ({ users, id, onDelete }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.filter(user => !user.removed).map(({ phone_number, active, user_id, user }) => {
+                    {users.filter(user => applyFilter(user, filter) && !user.removed).map(({ phone_number, active, user_id, user }) => {
                         return (
                             <tr key={user_id}>
                                 <td>{user ? user.name : ''}</td>
